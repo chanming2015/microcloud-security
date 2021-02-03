96523a54fb10f88270c27e5d4bbed12213132988
chanming2015 <476076812@qq.com>
2021-02-03 18:25:26 +0800

    bug fix.

--- src/main/java/com/github/chanming2015/microcloud/security/WebSecurityConfig.java
+++ src/main/java/com/github/chanming2015/microcloud/security/WebSecurityConfig.java
@@ -4,15 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 
 import com.github.chanming2015.microcloud.security.handler.MyAccessDecisionManager;
+import com.github.chanming2015.microcloud.security.handler.MyAuthenctiationFailureHandler;
+import com.github.chanming2015.microcloud.security.handler.MyAuthenctiationSuccessHandler;
+import com.github.chanming2015.microcloud.security.handler.MyLogoutSuccessHandler;
 import com.github.chanming2015.microcloud.security.util.SecurityUtil;
 
 /**
@@ -24,6 +25,9 @@ import com.github.chanming2015.microcloud.security.util.SecurityUtil;
 @Configuration
 public class WebSecurityConfig
 {
+    @Autowired
+    private MyAuthenctiationSuccessHandler myAuthenctiationSuccessHandler;
+
     @Autowired
     private MyAccessDecisionManager myAccessDecisionManager;
 
@@ -40,14 +44,10 @@ public class WebSecurityConfig
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http)
     {
-        http.oauth2Login().and().authorizeExchange().pathMatchers(HttpMethod.GET, "/.public/jwks.json").permitAll().anyExchange().access(myAccessDecisionManager);
+        http.formLogin().authenticationSuccessHandler(myAuthenctiationSuccessHandler).authenticationFailureHandler(new MyAuthenctiationFailureHandler());
+        http.logout().logoutSuccessHandler(new MyLogoutSuccessHandler());
+        http.exceptionHandling().authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED));
+        http.authorizeExchange().pathMatchers(HttpMethod.GET, "/.public/jwks.json").permitAll().anyExchange().access(myAccessDecisionManager);
         return http.csrf().disable().build();
-    }
-
-    @Bean
-    public WebClient webClient(ReactiveClientRegistrationRepository clientRegistrationRepo, ServerOAuth2AuthorizedClientRepository authorizedClientRepo)
-    {
-        ServerOAuth2AuthorizedClientExchangeFilterFunction filter = new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepo, authorizedClientRepo);
-        return WebClient.builder().filter(filter).build();
     }
 }
